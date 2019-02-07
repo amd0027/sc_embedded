1cd456f05271d5179789d10a43c31fbbba455a98
Alex Dillon <alexdillon@knology.net>
2019-02-07 17:12:00 -0600

    Added SCDataClient with support for posting Heart Rate data through the web API

---
+++ main/api/SCDataClient.cpp
@@ -0,0 +1,50 @@
+/*
+ * SCDataClient.cpp
+ *
+ *  Created on: Feb 7, 2019
+ *      Author: Alex Dillon
+ */
+
+#include "SCDataClient.h"
+
+#include <cjson/cJSON.h>
+
+#include "webapi.h"
+
+const std::string SCDataClient::DATETIME_MIN_VALUE = "0001-01-01T00:00:00.000Z";
+
+SCDataClient::SCDataClient(std::string key):
+	authKey(key)
+{
+}
+
+bool SCDataClient::PostHeartRateData(HeartRateSensorModel data)
+{
+	cJSON* json = cJSON_CreateObject();
+
+	// for now, we are ignoring the timestamp field and substituting DateTime.MinValue
+	if (cJSON_AddStringToObject(json, "Timestamp", DATETIME_MIN_VALUE.c_str()) == NULL)
+	{
+		cJSON_Delete(json);
+		return false;
+	}
+
+	if (cJSON_AddNumberToObject(json, "MeasuredBPM", data.MeasuredBPM) == NULL)
+	{
+		cJSON_Delete(json);
+		return false;
+	}
+
+	char* jsonPayloadData = cJSON_Print(json);
+
+
+	int response = webapi::APIPostData(API_GET_URL(WEB_API_POST_HEARTRATE),
+			jsonPayloadData,
+			this->authKey.c_str());
+
+	ESP_LOGI(TAG, "Heart Rate POST Operation returned with status code %d", response);
+
+	return (response == 200);
+}
+
+
