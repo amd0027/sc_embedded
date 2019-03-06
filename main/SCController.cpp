f60441e1f238991e8ccb3cd09c7c753a6e4fe10d
Alex Dillon <alexdillon@knology.net>
2019-03-06 17:08:24 -0600

    Remove printf statements and replace with ESP Logging calls

--- main/SCController.cpp
+++ main/SCController.cpp
@@ -46,14 +46,14 @@ void SCController::Start()
 	/* Print chip information */
 	esp_chip_info_t chip_info;
 	esp_chip_info(&chip_info);
-	printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+	ESP_LOGI(TAG, "ESP32 chip with %d CPU cores, WiFi%s%s, ",
 			chip_info.cores,
 			(chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
 			(chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
-	printf("silicon revision %d, ", chip_info.revision);
+	ESP_LOGI(TAG, "ESP32 Silicon Revision %d, ", chip_info.revision);
 
-	printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+	ESP_LOGI(TAG, "Flash Size: %dMB %s", spi_flash_get_chip_size() / (1024 * 1024),
 			(chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
 	if (settings.auth_key.empty())
